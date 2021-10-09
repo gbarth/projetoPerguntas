4986c67a28efd36bcbc4ea03032b1e8209836169
Gustavo <gustavobartholomeucurso@gmail.com>
2021-10-09 18:48:28 -0300

    Início do projeto

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(new PerguntaApp());
+}
+
+class PerguntaApp extends StatelessWidget{
+  
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Text('Olá, Flutter!'),
+    );
+  }
+}
