e5aa7caea9cb80f90bac3a9339647b79498f1f6f
Gustavo <gustavobartholomeucurso@gmail.com>
2021-10-10 19:36:26 -0300

    Criação de botões de resposta

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,57 @@
 import 'package:flutter/material.dart';
+import './questao.dart';
 
 main(){
-  runApp(new PerguntaApp());
+  runApp(PerguntaApp());
 }
 
-class PerguntaApp extends StatelessWidget{
+class _PerguntaAppState extends State<PerguntaApp>{
+  var _perguntaSelecionada = 0;
+
+  void _responder(){
+    setState(() {
+      _perguntaSelecionada++;
+    });
+    print(_perguntaSelecionada);
+  }
   
   @override
   Widget build(BuildContext context){
+    final perguntas = [
+      'Qual sua cor favorita?',
+      'Qual seu animal favorito?',
+    ];
+    
     return MaterialApp(
-      home: Text('Olá, Flutter!'),
-    );
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Perguntas'),
+          ),
+          body: Column(
+            children: [
+              Questao(perguntas[_perguntaSelecionada]),
+              ElevatedButton(
+                child: const Text('Resposta 1'),
+                onPressed: _responder,
+              ),
+              ElevatedButton(
+                child: const Text('Resposta 2'),
+                onPressed: _responder,
+              ),
+              ElevatedButton(
+                child: const Text('Resposta 3'),
+                onPressed: _responder,
+              ),
+            ],
+          ), 
+        ),
+      );
+  }
+}
+
+class PerguntaApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return _PerguntaAppState();
   }
 }
